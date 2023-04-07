c490182a782b96ec84bdc6d1aafd3e7cdb7ab24b
Michael Houmann <mhoumann87@gmail.com>
2023-04-07 14:17:34 +0200

    Update ProductsProvider to use JSON server

--- src/context/ProductsProvider.tsx
+++ src/context/ProductsProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, createContext, useState } from 'react';
+import { ReactElement, createContext, useState, useEffect } from 'react';
 
 export type ProductType = {
   sku: string;
@@ -6,7 +6,8 @@ export type ProductType = {
   price: number;
 };
 
-const initState: ProductType[] = [
+// Static import of products
+/* const initState: ProductType[] = [
   {
     sku: 'item0001',
     name: 'widget',
@@ -22,7 +23,9 @@ const initState: ProductType[] = [
     name: 'Deluxe Widget',
     price: 29.95,
   },
-];
+]; */
+
+const initState: ProductType[] = [];
 
 export type UseProductsContextType = {
   products: ProductType[];
@@ -36,6 +39,21 @@ type ChildrenType = { children?: ReactElement | ReactElement[] };
 
 export const ProductsProvider = ({ children }: ChildrenType): ReactElement => {
   const [products, setProducts] = useState<ProductType[]>(initState);
+
+  useEffect(() => {
+    const fetchProducts = async (): Promise<ProductType[]> => {
+      const data = await fetch('http://localhost:3500/products')
+        .then(res => {
+          return res.json();
+        })
+        .catch(err => {
+          if (err instanceof Error) console.log(err.message);
+        });
+      return data;
+    };
+
+    fetchProducts().then(products => setProducts(products));
+  }, []);
 
   return (
     <ProductsContext.Provider value={{ products }}>
